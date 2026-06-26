6d63747a3c179058d65b06eb0f693defa4d9db83
hectorandresladino <ing.andresladino@proton.me>
2026-06-26 14:25:01 -0500

    Corregir configuracion Capacitor para Android

--- frontend-react/android/app/src/main/java/com/phtransparente/app/MainActivity.java
+++ frontend-react/android/app/src/main/java/com/phtransparente/app/MainActivity.java
@@ -1,11 +1,15 @@
 package com.phtransparente.app;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Habilitar debugging de WebView
+        WebView.setWebContentsDebuggingEnabled(true);
     }
 }
